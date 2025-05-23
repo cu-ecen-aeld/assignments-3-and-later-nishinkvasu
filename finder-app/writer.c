8b5ea86ba2459b6c133857dc851e325a6eec6d28
nkv <nishin1@e.ntu.edu.sg>
2025-05-23 17:35:59 -0400

    Contents for the assignment2
    Files added:
    Txt files showing config and version of cross compiler; output of file on cross compiled writer app
    writer.c and associated makefile
    finder-test.sh updated for assignment-2

---
+++ finder-app/writer.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <syslog.h>
+// #include <sys/types.h>
+// #include <sys/stat.h>
+
+
+int main(int argc, char**argv){
+    int fd;
+    ssize_t nr;
+
+    openlog(NULL, 0, LOG_USER);
+    // setlogmask(LOG_MASK(LOG_EMERG)|LOG_MASK(LOG_ALERT)|LOG_MASK(LOG_CRIT)|LOG_MASK(LOG_ERR)|LOG_MASK(LOG_WARNING));
+    setlogmask(LOG_UPTO(LOG_INFO));
+    // syslog(LOG_INFO, "Info: Testing syslog - round2!!");
+   
+    if(argc < 2){
+        // printf("Err: Less than two arguments are passed!\n");
+        syslog(LOG_ERR, "Err: Less than two arguments are passed!");
+        return 1;
+    }
+    printf("%s\n", argv[1]);
+    fd = open(argv[1], O_RDWR | O_CREAT, 0644);
+    if (fd < 0){
+        // printf("Err: File could not be opened!\n");
+        syslog(LOG_ERR, "Err: File \"%s\" could not be opened!!", argv[1]);
+        return 1;
+    }
+
+    nr = write(fd, (const void *)argv[2], strlen(argv[2]));
+    if (nr < 0){
+        // printf("Err: Write Error! \n");
+        syslog(LOG_ERR, "Err: Write Error! ");
+        return 1;
+    }
+    else if(nr != strlen(argv[2])){
+        // printf("Err: Only Partial Write done - %d bytes", nr);
+        syslog(LOG_ERR, "Err: Only Partial Write done - %ld bytes", nr);
+        return 1;
+    }
+
+    return 0;
+}
