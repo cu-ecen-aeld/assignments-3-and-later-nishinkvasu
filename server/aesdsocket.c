964beefe09eb0cee111a821f0119d6c1ee2d5767
Nishin K Vasu <nishin1@e.ntu.edu.sg>
2025-11-04 14:12:20 -0500

    Working version, need to review and refactor

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -7,8 +7,22 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <syslog.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <signal.h>
 
 #define AESD_PORT 9000
+
+
+static bool signal_caught = false;
+
+static void signal_handler(int signal_number)
+{
+    int errno_saved = errno;
+    if((signal_number == SIGINT) || (signal_number == SIGTERM))
+        signal_caught = true;
+    errno = errno_saved;
+}
 
 int main(int argc, char **argv)
 {
@@ -32,6 +46,19 @@ int main(int argc, char **argv)
 
     int sockfd;
     struct sockaddr_in my_addr;
+
+    signal_caught = false;
+    // set up signal handler
+    struct sigaction new_sigaction;
+    memset(&new_sigaction, 0, sizeof (struct sigaction));
+    new_sigaction.sa_handler = signal_handler;
+
+    if( sigaction(SIGTERM, &new_sigaction, NULL) != 0){
+        printf("Error registering for SIGTERM");
+    }
+    if( sigaction(SIGINT, &new_sigaction, NULL) != 0){
+        printf("Error registering for SIGTERM");
+    }
 
     sockfd = socket(PF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
@@ -60,88 +87,204 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
+    // daemonize if the option is passed
+    #define DAEMONIZE   "-d"
+    printf("argc = %d\n",argc);
+    if(argc == 2)
+    {
+        if(strncmp(argv[1], DAEMONIZE, strlen(DAEMONIZE)) == 0)  
+        {
+            // fork
+            // exit parent
+            // setsid - create new session to ensure no controlling tty
+            // chdir to "/"
+            // close fds
+            pid_t pid = fork();
+            if(pid < 0)
+                return -1; // fork failed
+
+            if(pid > 0)
+            {
+                printf("Exiting parent process - child pid %d \n", pid);
+                exit(0);
+            }
+
+            if(setsid() < 0)
+                exit(1);
+
+            if (chdir("/") < 0) 
+                exit(1);
+
+            close(STDIN_FILENO);
+            close(STDOUT_FILENO);
+            close(STDERR_FILENO);
+
+            open("/dev/null", O_RDWR);
+            dup(0);
+            dup(0);
+            
+        }
+    }
+
 #define BACKLOG 5 // pending connections allowed
+
     if (listen(sockfd, BACKLOG) < 0)
     {
         perror("listen failed");
         printf("listen failed \n");
+        syslog(LOG_ERR, "AESD: Listen failed!");
         exit(EXIT_FAILURE);
     }
-
+    syslog(LOG_ERR, "AESD: before open and accept!");
     int newsockfd;
     socklen_t addr_size;
     struct sockaddr_storage clientaddr;
     addr_size = sizeof clientaddr;
 
     // open file for storing the collected data
+    // char* fileName = "aesdsocketdata"; 
+    // while creating daemon the above will fail as we change to the root directory
+    char* fileName = "/var/tmp/aesdsocketdata"; 
 
-    int filefd = open("aesdsocketdata", O_RDWR | O_CREAT, 0644);
+
+    int filefd = open(fileName, O_RDWR | O_CREAT, 0644);
     if (filefd < 0)
     {
         // printf("Err: File could not be opened!\n");
-        syslog(LOG_ERR, "Err: File \"%s\" could not be opened!!", argv[1]);
-        return 1;
+        syslog(LOG_ERR, "AESD: File \"%s\" could not be opened!!", fileName);
+        exit(EXIT_FAILURE);
     }
     while (1)
     {
+        if(signal_caught == true){
+            close(sockfd);
+            close(filefd); // had to close the file before reading back again
+
+            // delete var/tmp/aesdsocketdata
+        }
         printf("before accept \n");
         // accept should also be in a loop and then should handle SIGINT
         if ((newsockfd = accept(sockfd, (struct sockaddr *)&clientaddr, &addr_size)) < 0)
         {
+            if(newsockfd == -1)
+            {
+                if((errno == EINTR) && (signal_caught == true))
+                {
+                    close(sockfd);
+                    close(filefd);
+
+                    if(!remove(fileName))
+                        printf("File remove failed \n");
+
+                    printf("accept failed due to SIGINT or SIGTERM\n");
+                }
+            }
             perror("accept failed");
             printf("accept failed \n");
+            syslog(LOG_ERR, "AESD: accept failed!!");
+
             exit(EXIT_FAILURE);
         }
 
-        char buf[10];
+        char buf[512];
         int recv_count;
 
         // receive data in a while loop to ensure big buffers of data can be handled
         // while(1)
         // {
+        ssize_t nr;
         do
         {
-            printf("before recv \n");
+            // printf("before recv \n");
             recv_count = recv(newsockfd, buf, sizeof buf, 0);
             if (recv_count > 0)
             {
                 char *tmpbuf = (char *)malloc((sizeof buf) + 1);
                 memset(tmpbuf, 0, (sizeof buf) + 1);
                 strncpy(tmpbuf, (const char *)&buf, recv_count);
-                tmpbuf[recv_count + 1] = '\0';
+                tmpbuf[recv_count] = '\0';
                 printf("string received %s\n", tmpbuf);
                 free(tmpbuf);
 
                 // write the buffer into the file
                 // another option is to store it into a big enough buffer and write it at last
                 // will need to see what happens if multiple buffers are written if it introduces a new line
-                ssize_t nr = write(filefd, (const void *)buf, recv_count);
+                nr = write(filefd, (const void *)buf, recv_count);
                 if (nr < 0)
                 {
                     // printf("Err: Write Error! \n");
-                    syslog(LOG_ERR, "Err: Write Error! ");
+                    syslog(LOG_ERR, "AESD: Write Error! ");
                     return 1;
                 }
                 else if (nr != recv_count)
                 {
                     // printf("Err: Only Partial Write done - %d bytes", nr);
-                    syslog(LOG_ERR, "Err: Only Partial Write done - %ld bytes", nr);
+                    syslog(LOG_ERR, "AESD: Only Partial Write done - %ld bytes", nr);
                     return 1;
                 }
             }
             else if (recv_count == 0)
             {
+                syslog(LOG_ERR, "AESD: client disconnected!! ");
                 printf("client disconnected!!\n");
+                break;  // added precaution to break out of infinite loop
+                        // although code should not reach here ideally
             }
             else
             {
+                syslog(LOG_ERR, "AESD: recv failed!! ");
                 perror("recv failed");
+                break;
             }
-        } while (recv_count > 0);
+        // } while (recv_count > 0);
+        } while (buf[recv_count-1] != '\n');
 
+        // fsync(filefd); // way to flush to disk
+        close(filefd); // had to close the file before reading back again
+        filefd = open(fileName, O_RDWR , 0644);
+        if (filefd < 0)
+        {
+            // printf("Err: File could not be opened!\n");
+            syslog(LOG_ERR, "AESD: File \"%s\" could not be opened!!", fileName);
+            return 1;
+        }
+        // Send the file contents back from here
+        // Will need to read the file contents till we reach a newline
+        printf("reading file!!\n");
+        memset(buf, 0, (sizeof buf));
+        do{
+            nr = read(filefd, buf, sizeof(buf));
+            if (nr < 0)
+            {
+                printf("Err: Read Error! \n");
+                syslog(LOG_ERR, "AESD: Read Error! ");
+                return 1;
+            }
+
+            // Verifying read part below
+            char *tmpbuf = (char *)malloc((sizeof buf) + 1);
+            memset(tmpbuf, 0, (sizeof buf) + 1);
+            strncpy(tmpbuf, (const char *)&buf, nr);
+            tmpbuf[nr] = '\0';
+            printf("%s", tmpbuf);
+            // printf("%d string read %s", (int)nr, tmpbuf);
+            free(tmpbuf);
+
+            // send file contents back over the socket
+            // ssize_t send(int s, const void *buf, size_t len, int flags);
+            if(nr > 0)
+                if (send(newsockfd, buf, nr, 0) == -1){
+                    syslog(LOG_ERR, "AESD: send Error! ");
+                    perror("send");
+                }
+                // ssize_t sendcount = 
+            
+        }while (nr > 0);
         close(newsockfd);
     }
 
     close(sockfd);
     close(filefd);
+
+    return EXIT_FAILURE;
 }
